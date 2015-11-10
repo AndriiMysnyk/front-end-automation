a3d3ccc378d38a386c4a61b889930f50a636eb2f
Andrii Mysnyk <andrii_mysnyk@epam.com>
2015-11-10 21:14:55 +0200

    using bootstrap

--- app/index.js
+++ app/index.js
@@ -1,10 +1,17 @@
 function buildCoverageList() {
+	var div = document.createElement('div'),
+		a, span;
 
 	for (var i = 0; i < coverage.length; i++) {
-		var a = document.createElement('a');
-		a.appendChild(document.createTextNode(coverage[i].title));
+		a = document.createElement('a');
+		a.appendChild(document.createTextNode(coverage[i].browser));
+		span = document.createElement('span');
+		span.appendChild(document.createTextNode(coverage[i].os));
+		span.className = "badge";
+		a.appendChild(span);
 		a.href = coverage[i].href;
-		document.body.appendChild(a);
-		document.body.appendChild(document.createElement('br'));
+		a.className = "list-group-item";
+		div.appendChild(a);
 	}
+	document.getElementById('reports').appendChild(div);
 }
